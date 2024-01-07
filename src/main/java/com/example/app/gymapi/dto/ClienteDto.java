71d9f0951f26d16ef809bcd873b5e73b9323607c
fernando <ffba97@gmail.com>
2024-01-07 16:42:32 -0300

    Definiendo valores no nulos

--- src/main/java/com/example/app/gymapi/dto/ClienteDto.java
+++ src/main/java/com/example/app/gymapi/dto/ClienteDto.java
@@ -1,13 +1,18 @@
 package com.example.app.gymapi.dto;
 
 import com.example.app.gymapi.abstracts.AbstractDto;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.util.Date;
 
 @Data
 public class ClienteDto extends AbstractDto {
+    @NotNull
     private String nombre;
+    @NotNull
     private String cedula;
+    @NotNull
     private String ruc;
     private String telefono;
     private String email;
