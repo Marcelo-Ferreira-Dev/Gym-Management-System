0f5a9a87f678676eb42cf6d2d50c1c18f48f6385
fernando <ffba97@gmail.com>
2024-01-08 12:27:00 -0300

    Corrección ClienteDao

--- src/main/java/com/example/app/gymapi/dao/ClienteDao.java
+++ src/main/java/com/example/app/gymapi/dao/ClienteDao.java
@@ -1,13 +1,15 @@
 package com.example.app.gymapi.dao;
 
 import com.example.app.gymapi.bean.clientes.Cliente;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
 
-import java.awt.print.Pageable;
-
+@Repository
 public interface ClienteDao extends JpaRepository<Cliente, Long> {
-    public Cliente findAllByActivoIsTrue(String email);
-    public Cliente findByEmailAndActivoIsTrue(Pageable pag, String email);
-    public Cliente findByCedulaAndActivoIsTrue(String cedula);
-    public Cliente findByRucAndActivoIsTrue(Pageable pag, String ruc);
+    public Page<Cliente> findAllByActivoIsTrue(Pageable pag, String email);
+    public Page<Cliente> findByEmailAndActivoIsTrue(Pageable pag, String email);
+    public Page<Cliente> findByCedulaAndActivoIsTrue(Pageable pageable, String cedula);
+    public Page<Cliente> findByRucAndActivoIsTrue(Pageable pag, String ruc);
 }
